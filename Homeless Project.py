4d5070c20813820dff5264ffd43ac4b3b140c979
Nanako Ohashi <nanako.ohashi@gmail.com>
2019-03-08 10:21:49 -0700

    refactor: rename df_population columns
    Rename df_population columns for better clarity.

--- Homeless Project.py
+++ Homeless Project.py
@@ -20,12 +20,12 @@ with zipfile.ZipFile('homelessness.zip', 'r') as myzip:
 # ## Assess
 # 
 
-# In[4]:
+# In[14]:
 
 
-# dataset 1 -
+# dataset 1 - homelessness data
 df_homeless = pd.read_csv('2007-2016-Homelessnewss-USA.csv')
-df_homeless.head()
+df_homeless
 
 
 # In[11]:
@@ -34,9 +34,10 @@ df_homeless.head()
 df_homeless.info()
 
 
-# In[9]:
+# In[13]:
 
 
+# dataset 2 - population data
 df_population = pd.read_csv('Population-by-state.csv')
 df_population.head()
 
@@ -47,8 +48,16 @@ df_population.head()
 df_population.info()
 
 
-# In[ ]:
+# - States are abbreviated for df_homeless but are in full in population.
+# - Change 'Geography' to 'State' for df_population.
+# - Rename columns for df_population for better clarity.
+
+# ## Clean
+
+# #### Define
+
+# In[22]:
 
 
-
-
+df_population1 = df_population.rename(columns={'GEO.display-label': 'State', 'respop72010': 'Population_2010', 'respop72011': 'Population_2011', 'respop72012': 'Population_2012', 'respop72013': 'Population_2013', 'respop72014': 'Population_2014', 'respop72015': 'Population_2015', 'respop72016': 'Population_2016'})
+df_population1
