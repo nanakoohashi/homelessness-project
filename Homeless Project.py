f75512e0dda5deb3a4b52fab0a0ad0375859fd96
Nanako Ohashi <nanako.ohashi@gmail.com>
2019-06-16 15:19:10 -0600

    feat: add heading

--- Homeless Project.py
+++ Homeless Project.py
@@ -326,7 +326,7 @@ df_combined.loc['Diff'].sort_values()
 diff_homeless_percent.to_csv('diff_homeless.csv')
 
 
-### United States Change in Homeless Population from 2010 to 2016
+# ### United States Change in Homeless Population from 2010 to 2016
 
 
 # In[ ]:
@@ -345,6 +345,7 @@ diff_homeless_percent
 <div class='tableauPlaceholder' id='viz1553058833424' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CY&#47;CYPPT8672&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='path' value='shared&#47;CYPPT8672' /> <param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CY&#47;CYPPT8672&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1553058833424');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.minWidth='1000px';vizElement.style.maxWidth='100%';vizElement.style.minHeight='587px';vizElement.style.maxHeight=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
 
 
+# ### United States Homeless Population (as a Percent of Total Population) in 2010
 # In[ ]:
 
 #Import Tableau Dashboard to Jupyter Notebooks (Percentage Homeless in 2010)
