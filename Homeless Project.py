c216b32c8ca358b0a931698b128ec4dfd96bcb6b
Nanako Ohashi <nanako.ohashi@gmail.com>
2019-06-23 11:38:59 -0600

    feat: add summary

--- Homeless Project.py
+++ Homeless Project.py
@@ -2,6 +2,8 @@
 # coding: utf-8
 
 # # How has Homelessness Changed in the US from 2010 to 2019?
+# This dataset reports the national estimates of homelessness by state from 2007 - 2018. Estimates of homeless veterans are included from the beginning of 2011. This dataset was obtained from [**HUD EXCHANGE**](https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/).
+
 
 # In[22]:
 
